c630a04cf50e285f1f5797f865044d0d96fc57e4
mbarocho <mbarocho@udel.edu>
2023-11-30 19:47:53 -0500

    Added firebase compatibility

--- firebase/index.js
+++ firebase/index.js
@@ -1,3 +1,29 @@
+var admin = require("firebase-admin");
+
+var serviceAccount = require("/etc/secrets/fir-test-437e7-firebase-adminsdk-mpdcr-98d8453e41.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert("./fir-test-437e7-firebase-adminsdk-mpdcr-98d8453e41.json"),
+  databaseURL: "https://fir-test-437e7-default-rtdb.firebaseio.com"
+});
+
+// Get a database reference to our blog
+const { getDatabase } = require('firebase-admin/database');
+const db = getDatabase();
+const ref = db.ref('server/saving-data/');
+const usersRef = ref.child('users');
+usersRef.set({
+    alanisawesome: {
+        date_of_birth: 'June 23, 1912',
+        full_name: 'Alan Turing'
+    },
+    gracehop: {
+        date_of_birth: 'December 9, 1906',
+        full_name: 'Grace Hopper'
+    }
+});
+
+
 const http = require('http')
 const port = 8080
 // Create a server object:
